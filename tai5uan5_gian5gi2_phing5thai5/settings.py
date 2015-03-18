adff4f467ad224568d0b6ec7eea6d06337a93f67
薛丞宏 <ihcaoe@gmail.com>
2015-03-18 14:50:33 +0800

    會使振動 丞宏

--- tai5uan5_gian5gi2_phing5thai5/settings.py
+++ tai5uan5_gian5gi2_phing5thai5/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -36,6 +36,15 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+# 	allauth
+	# The Django sites framework is required for allauth
+	'django.contrib.sites',
+	'allauth',
+	'allauth.account',
+	'allauth.socialaccount',
+	'allauth.socialaccount.providers.facebook',
+	'allauth.socialaccount.providers.google',
+# 	家己的
 	'臺灣言語資料庫',
 	'臺灣言語平臺',
 )
@@ -83,3 +92,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+	# Required by allauth template tags
+	"django.core.context_processors.request",
+	# allauth specific context processors
+	"allauth.account.context_processors.account",
+	"allauth.socialaccount.context_processors.socialaccount",
+	'django.contrib.auth.context_processors.auth',
+)
+AUTHENTICATION_BACKENDS = (
+	# Needed to login by username in Django admin, regardless of `allauth`
+	"django.contrib.auth.backends.ModelBackend",
+	# `allauth` specific authentication methods, such as login by e-mail
+	"allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SOCIALACCOUNT_PROVIDERS = \
+	{'facebook':
+		{'SCOPE': ['email', ],
+		'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+		'METHOD': 'oauth2',
+		'LOCALE_FUNC': 'path.to.callable',
+		'VERIFIED_EMAIL': False,
+		'VERSION': 'v2.2',
+		}
+	}
+   
+SITE_ID = 1
--- tai5uan5_gian5gi2_phing5thai5/urls.py
+++ tai5uan5_gian5gi2_phing5thai5/urls.py
@@ -1,10 +1,31 @@
 from django.conf.urls import patterns, include, url
-# from django.contrib import admin
+from django.contrib import admin
+from django.views.generic.base import TemplateView
+from django.http.response import HttpResponse
+from django.contrib.auth.views import login
+from django.contrib.auth import authenticate, get_user_model
+from allauth.account.views import logout
+admin.autodiscover()
+User=get_user_model()
+def XX(request):
+	logout(request)
+	a=User.objects.get(pk=1)
+	login(request,User.objects.get(pk=1))
+	print(a)
+	return XXX(request)
+def XXX(request):
+	return HttpResponse('{} X {} X {} X {}'.format(str(request.user),
+			str(request.user.pk),request.user.username,request.user.pk==None))
 
 urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'tai5uan5_gian5gi2_phing5thai5.views.home', name='home'),
+	url(r'^accounts/', include('allauth.urls')),
+	url(r'^$', TemplateView.as_view(template_name='index.html')),
+	url(r'^accounts/profile/$', TemplateView.as_view(template_name='profile.html')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^XX',XX),
+	
+	url(r'^admin/', include(admin.site.urls)),
 	url(r'^', include('臺灣言語平臺.網址')),
-
-#	 url(r'^admin/', include(admin.site.urls)),
 )
